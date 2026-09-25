ca56ba3fde16f38d8a90c58d034e0a4b7ec26dee
gs sys <gurusuryd10@gmail.com>
2026-09-25 16:07:49 +0530

    bitbucket

--- app.py
+++ app.py
@@ -15,16 +15,16 @@ print(l)
 
 #  FIBONACCI SERIS
 
-# def f(n):
-#     if n == 0:
-#         return 0
-#     elif n == 1:
-#         return 1
-#     else:
-#         return f(n-1) + f(n-2)
-#
-# for i in range(1,10):
-#     print(f(i))
+def f(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return f(n-1) + f(n-2)
+
+for i in range(1,10):
+    print(f(i))
 
 # REVERSE THE LIST
 
